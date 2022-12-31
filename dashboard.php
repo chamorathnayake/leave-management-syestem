3c3c232e84405bdd6b37f8fc920ad574279b8a23
chamorathnayake <93019022+chamorathnayake@users.noreply.github.com>
2022-12-31 11:04:47 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,327 @@
+<?php 
+	include('inc/head.php'); 
+	session_start();
+
+	if (isset($_SESSION['epf'])) {
+		
+	}
+	else{
+		header('location:index.php');
+	}
+
+?>
+<body>
+	<nav class="navbar navbar-toggleable-sm navbar-inverse bg-inverse p-0">
+		<div class="container">
+			<button class="navbar-toggler toggler-right" data-target="#mynavbar" data-toggle="collapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a href="#" class="navbar-brand mr-3">Online Leave Management System</a>
+			<div class="collapse navbar-collapse" id="mynavbar">
+				<ul class="navbar-nav">
+					<li class="nav-item px-2"><a href="#" class="nav-link active">Logged in as <?php echo $_SESSION['epf']?></a></li>
+					
+				</ul>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item dropdown mr-3">
+						
+						<li class="nav-item">
+							<a href="logout.php" class="nav-link"><i class="fa fa-power-off"></i> Logout</a>
+						</li>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!--This is section-->
+
+	<section id="sections" class="py-4 mb-4 bg-faded">
+		<div class="container">
+			<div class="row">
+				<div class="col-md"></div>
+				<div class="col-md-3">
+					<a href="#" class="btn btn-primary btn-block" style="border-radius:0%;" data-toggle="modal" data-target="#addPostModal"><i class="fa fa-plus"></i> Apply Leave</a>
+				</div>
+				<div class="col-md-3">
+					<a href="#" class="btn btn-warning btn-block" style="border-radius:0%;" data-toggle="modal" data-target="#addCateModal"><i class="fa fa-spinner"></i> Pendings</a>
+				</div>
+				<div class="col-md-3">
+					<a href="#" class="btn btn-success btn-block" style="border-radius:0%;" data-toggle="modal" data-target="#addUsertModal"><i class="fa fa-check"></i> Approved Leaves</a>
+				</div>
+				<div class="col-md"></div>
+			</div>
+		</div>
+	
+	</section>
+	<!----Section2 for showing Post Model ---->
+	<section id="post">
+		<div class="container">
+			<div class="row">
+			<table class="table table-bordered table-hover table-striped">
+							<thead>
+								<th>#</th>
+								<th>Name</th>
+								<th>Department</th>
+								<th>Leave Date</th>
+								<th>Leave TO</th>
+								<th>Reason</th>
+								<th>Status</th>
+							</thead>
+							 <tbody>
+							 	<?php 
+									$sql = "SELECT * FROM leaves WHERE epf='".$_SESSION['epf']."'";
+									$que = mysqli_query($con,$sql);
+									$cnt=1;
+									while ($result = mysqli_fetch_assoc($que)) {
+									?>
+
+									
+							 	<tr>
+									<td><?php echo $cnt;?></td>
+							 		<td><?php echo $result['name']; ?></td>
+							 		<td><?php echo $result['department']; ?></td>
+							 		<td><?php echo $result['leavedate']; ?></td>
+									 <td><?php echo $result['leavedateto']; ?></td>
+							 		<td><?php echo $result['leavereason']; ?></td>
+							 		<td>
+							 			<?php 
+							 			if ($result['status'] == 0) {
+							 				echo "<span class='badge badge-warning'>Pending</span>";
+							 			}
+							 		    elseif ($result['status'] == 1){
+							 				echo "<span class='badge badge-success'>Approved</span>";
+							 			}
+										 else{
+											echo "<span class='badge badge-danger'>Rejected</span> ";
+										}
+									   
+							 	$cnt++;	}
+							 		 ?>
+							 		</td>
+							 	</tr>
+
+							 </tbody>
+						</table>
+			</div>
+		</div>
+	</section>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<!----Section3 footer ---->
+	<section id="main-footer" class="text-center text-white bg-inverse mt-4 p-4">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				<p class="lead">&copy; <?php echo date("Y")?> NML</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<!-- Creating Modal -->
+    <!-- Header Post -->
+	<div class="modal fade" id="addPostModal">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header bg-primary text-white">
+					<div class="modal-title">
+						<h5>Apply Leave</h5>
+					</div>
+					<button class="close" data-dismiss="modal"><span>&times;</span></button>
+				</div>
+				<div class="modal-body">
+					<form action="" method="post">
+						<div class="form-group">
+							<label class="form-control-label">Name</label>
+							<input type="text" name="name"class="form-control"/>
+							<input type="hidden" name="epf" value="<?php echo $_SESSION['epf']?>">
+							<label class="form-control-label">Department</label>
+							<select name="department" class="form-control" >
+							<option value="Admin">Admin</option>
+								<option value="HR">HR</option>
+								<option value="Workstudy">Workstudy</option>
+								<option value="Quality">Quality</option>
+								<option value="Packing">Packing</option>
+								<option value="Stores">Stores</option>
+								<option value="Cutting">Cutting</option>
+								<option value="Embrodering">Embrodering</option>
+								<option value="Management">Management</option>
+								<option value="Production">Production</option>
+							</select>
+						</div>
+						
+						<div class="form-group">
+							<label class="form-control-label">Leave Date</label>
+							<input type="date" name="leavedate" class="form-control" />
+						</div>
+						<div class="form-group">
+							<label class="form-control-label">Leave Date To</label>
+							<input type="date" name="leavedateto" class="form-control" />
+						</div>
+						<div class="form-group">
+							<label>Reason For Leave</label>
+							<textarea name="editor1" class="form-control"></textarea>
+						</div>
+				</div>
+				<div class="modal-footer">
+					<button class="btn btn-danger" style="border-radius:0%;" data-dismiss="modal">Close</button>
+					<input type="hidden" name="status" value="0">
+					<input type="submit" class="btn btn-success" style="border-radius:0%;" name="apply"  value="Apply">
+				</div>
+			</form>
+			</div>
+		</div>
+	</div>
+	<!--Modal Category-->
+	<div class="modal fade" id="addCateModal">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header bg-warning text-white">
+					<div class="modal-title">
+						<h5>Pending Leaves</h5>
+					</div>
+					<button class="close" data-dismiss="modal"><span>&times;</span></button>
+				</div>
+				<div class="modal-body">
+				<table class="table table-bordered table-hover table-striped">
+							<thead>
+								<th>#</th>
+								<th>Name</th>
+								<th>Department</th>
+								<th>Leave Date</th>
+								<th>Leave TO</th>
+								<th>Reason</th>
+								<th>Status</th>
+							</thead>
+							 <tbody>
+							 	<?php 
+									$sql = "SELECT * FROM leaves WHERE status = 0 && epf='".$_SESSION['epf']."'";
+									$que = mysqli_query($con,$sql);
+									$cnt=1;
+									while ($result = mysqli_fetch_assoc($que)) {
+									?>
+
+									
+							 	<tr>
+									<td><?php echo $cnt;?></td>
+							 		<td><?php echo $result['name']; ?></td>
+							 		<td><?php echo $result['department']; ?></td>
+							 		<td><?php echo $result['leavedate']; ?></td>
+									 <td><?php echo $result['leavedateto']; ?></td>
+							 		<td><?php echo $result['leavereason']; ?></td>
+							 		<td>
+							 			<?php 
+							 			if ($result['status'] == 0) {
+											echo "<span class='badge badge-warning'>Pending</span>";
+							 			}
+							 			else{
+											echo "<span class='badge badge-success'>Approved</span>";
+							 			}
+							 	$cnt++;	}
+							 		 ?>
+							 		</td>
+							 	</tr>
+
+							 </tbody>
+						</table>
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<!-- User Modal -->
+	<div class="modal fade" id="addUsertModal">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header bg-success text-white">
+					<div class="modal-title">
+						<h5>Total Approved Leaves</h5>
+					</div>
+					<button class="close" data-dismiss="modal"><span>&times;</span></button>
+				</div>
+				<div class="modal-body">
+				<table class="table table-bordered table-hover table-striped">
+							<thead>
+								<th>#</th>
+								<th>Name</th>
+								<th>Department</th>
+								<th>Leave Date From</th>
+								<th>Leave Date To</th>
+								<th>Reason</th>
+								<th>Status</th>
+							</thead>
+							 <tbody>
+							 	<?php 
+									$sql = "SELECT * FROM leaves WHERE status = 1 AND epf='".$_SESSION['epf']."'";
+									$que = mysqli_query($con,$sql);
+									$cnt=1;
+									while ($result = mysqli_fetch_assoc($que)) {
+									?>
+
+									
+							 	<tr>
+									<td><?php echo $cnt;?></td>
+							 		<td><?php echo $result['name']; ?></td>
+							 		<td><?php echo $result['department']; ?></td>
+							 		<td><?php echo $result['leavedate']; ?></td>
+									 <td><?php echo $result['leavedateto']; ?></td>
+							 		<td><?php echo $result['leavereason']; ?></td>
+							 		<td>
+							 			<?php 
+							 			if ($result['status'] == 0) {
+											echo "<span class='badge badge-warning'>Pending</span>";
+							 			}
+							 			else{
+											echo "<span class='badge badge-success'>Approved</span>";
+							 			}
+							 	$cnt++;	}
+							 		 ?>
+							 		</td>
+							 	</tr>
+
+							 </tbody>
+						</table>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+  
+  
+  <script src="js/jquery.min.js"></script>
+  <script src="js/tether.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://cdn.ckeditor.com/4.9.1/standard/ckeditor.js"></script>
+  <script>
+	CKEDITOR.replace('editor1');
+  </script>
+</body>
+</html>
+<?php 
+	if (isset($_POST['apply'])){
+		$name = $_POST['name'];
+		$epf = $_POST['epf'];
+		$department = $_POST['department'];
+		$leavedate = $_POST['leavedate'];
+		$leavedateto = $_POST['leavedateto'];
+		$editor1 = $_POST['editor1'];
+		$status = $_POST['status'];
+
+		$sql = "INSERT INTO leaves(name,epf,department,leavedate,leavedateto,leavereason,status)VALUES('$name','$epf','$department','$leavedate','$leavedateto','$editor1','$status')";
+
+		$run = mysqli_query($con,$sql);
+
+		if($run == true){
+			
+			echo "<script> 
+					alert('Leave Requested, Please wait for approval status');
+					window.open('dashboard.php','_self');
+				  </script>";
+		}else{
+			echo "<script> 
+			alert('Failed To Apply');
+			</script>";
+		}
+	}
+	
+ ?>
